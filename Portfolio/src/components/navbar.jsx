67ab921ef00b2dbcddb7a9bc2deaa9a1b54d1f33
jeet <jeetproxy@gmail.com>
2025-08-16 18:35:15 +0530

    initialize files

---
+++ Portfolio/src/components/navbar.jsx
@@ -0,0 +1,13 @@
+export function NavBar() {
+
+  return (
+    <>
+    <div className="navbar">
+      <nav>
+        <h3>My Portfolio</h3>
+      </nav>
+    </div>
+    </>
+  )
+}
+
---
+++ Portfolio/src/pages/portfolio.jsx
@@ -0,0 +1,16 @@
+import { NavBar } from '../components/navbar'
+import './portfolio.css'
+
+function PortfolioPage() {
+
+  return (
+    <>
+    <NavBar />
+    <p>
+        my portfolio
+    </p>
+    </>
+  )
+}
+
+export default PortfolioPage
