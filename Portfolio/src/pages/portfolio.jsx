0ed4bf0a173235a0c2dab00050aeb3ffb1e1832d
jeet <jeetproxy@gmail.com>
2025-09-18 19:31:33 +0530

    added framer motion anims

--- Portfolio/src/pages/portfolio.jsx
+++ Portfolio/src/pages/portfolio.jsx
@@ -4,6 +4,7 @@ import amazonCloneImg from '../assets/Amazon-clone-ss.png'
 import {useRef} from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCss3Alt, faHtml5, faJs, faPython, faReact } from '@fortawesome/free-brands-svg-icons';
+import { motion } from 'framer-motion'
 import './portfolio.css'
 
 function PortfolioPage() {
@@ -17,16 +18,25 @@ function PortfolioPage() {
   return (
     <>
     <NavBar introRef={introRef} skillsRef={skillsRef} projectsRef={projectsRef} educationRef={educationRef} contactRef={contactRef} />
-    <div ref={introRef} className='intro'>
+    <motion.div 
+        initial={{ opacity: 0, x: -100 }}
+        whileInView={{ opacity: 1, x: 0 }}
+        transition={{ duration: 0.6 }}
+    
+    ref={introRef} className='intro'>
       <div className="intro-row">
-        <h1>
+        <motion.h1 
+          initial={{ opacity: 0, y: 0 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.8 }}
+        >
           I am Jeet Dhali. I like to design my website using art and animations.<br />
           I feel minimalist UI design are good but adding art and animations make <br /> the website feel more alive and lively.
           I like game dev so I try to put my <br /> game dev  knowledge to my website to improve the UI/UX.
-        </h1>
+        </motion.h1>
         <Coffee />
       </div>
-    </div>
+    </motion.div>
 
     <div ref={skillsRef} className='skills'>
       <h1>Skills</h1>
@@ -35,7 +45,7 @@ function PortfolioPage() {
           <span className="skill"><FontAwesomeIcon icon={faCss3Alt} size="2x" /></span>
           <span className="skill"><FontAwesomeIcon icon={faJs} size="2x" /></span>
           <span className="skill"><FontAwesomeIcon icon={faReact} size="2x" /></span>
-          <span className="skill"><h3>C++</h3></span>
+          <span className="skill"><span style={{ marginTop: '6px', marginBottom: '6px' }}>C++</span></span>
           <span className="skill"><FontAwesomeIcon icon={faPython} size="2x" /></span>
       </div>
     </div>
@@ -46,7 +56,7 @@ function PortfolioPage() {
         <div className='project-card'>
           <h2 className='project-name'>Amazon Clone</h2>
           <div className='project-img'>
-            <img src={amazonCloneImg}></img>
+            <a href="https://amazon-clone-backend-71hb.onrender.com"><img src={amazonCloneImg} /></a>
           </div>
             <p>This is a amazon clone with fully functional auth system and backend and database</p>
             <div className='project-links'>
